820dcb735cd98e9018b4bbb1ec4b42f7766d907e
Eric Kramer <theerickramer@gmail.com>
2019-03-18 12:12:38 -0700

    adds vue

--- server.rb
+++ server.rb
@@ -1,5 +1,10 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
+
+OMDB_KEY = ENV['OMDB_KEY']
+
+set :public_folder, settings.root + '/client/dist'
 
 get '/' do
-  "Hello World!"
+  send_file './client/dist/index.html'
 end
